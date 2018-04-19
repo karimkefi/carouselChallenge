8a9da55978f041cb3f15e0de69cdf98e8a7bd463
Karim Kefi <karimkefi@gmail.com>
2018-04-19 22:15:07 +0100

    Image caption working with moving slides appearing below

--- carousel.js
+++ carousel.js
@@ -4,6 +4,7 @@ var arrow = "right"
 
 function showImage(n) {
     var Images = document.getElementsByClassName("slideImg");
+    var Caption = document.getElementsByClassName("imgCaption");
 
     if (n > Images.length) {
         index = 1;
@@ -15,7 +16,8 @@ function showImage(n) {
 
     //Set every image to hidden
     for (i=0; i < Images.length; i++) {
-        Images[i].className ="slideImg hiddenImg";
+        Images[i].className = "slideImg hidden";
+        Caption[i].className = "imgCaption hidden"
     }
 
     console.log('Function before If Index: ' + index);
@@ -25,18 +27,25 @@ function showImage(n) {
     //what to do when images are sliding Right to Left
     if (arrow === "right") {
         if(start) {
-            Images[index - 1].className = "slideImg slideIN-RtoL";
+            Images[index - 1].className                 = "slideImg slideIN-RtoL";
+            Caption[index - 1].className                = "imgCaption caption-In";
             console.log('In (R to L): ' + (index-1));
         } else {
             if(index === 1) {
-                Images[Images.length - 1].className = "slideImg slideOut-RtoL";
+                Images[Images.length - 1].className     = "slideImg slideOut-RtoL";
+                Caption[Images.length - 1].className    = "imgCaption hidden";
                 console.log('Out (R to L): ' + (Images.length-1));
-                Images[index - 1].className         = "slideImg slideIN-RtoL";
+
+                Images[index - 1].className             = "slideImg slideIN-RtoL";
+                Caption[index - 1].className            = "imgCaption caption-In";
                 console.log('In (R to L): ' + (index-1));
             } else {
-                Images[index - 2].className         = "slideImg slideOut-RtoL";
+                Images[index - 2].className             = "slideImg slideOut-RtoL";
+                Caption[index - 2].className            = "imgCaption hidden";
                 console.log('Out (R to L): ' + (index-2));
-                Images[index - 1].className         = "slideImg slideIN-RtoL";
+
+                Images[index - 1].className             = "slideImg slideIN-RtoL";
+                Caption[index - 1].className            = "imgCaption caption-In";
                 console.log('In (R to L): ' + (index-1));
             }
         }
@@ -45,18 +54,25 @@ function showImage(n) {
     //what to do when images are sliding Left to Right
     if(arrow === "left") {
         if(start) {
-            Images[index - 1].className = "slideImg slideIN-LtoR";
+            Images[index - 1].className                 = "slideImg slideIN-LtoR";
+            Caption[index - 1].className                = "imgCaption caption-In";
             console.log('In (L to R): ' + (index-1));
         } else {
             if(index === 5) {
-                Images[Images.length - 1].className = "slideImg slideIN-LtoR";
+                Images[Images.length - 1].className     = "slideImg slideIN-LtoR";
+                Caption[Images.length - 1].className    = "imgCaption caption-In";
                 console.log('In (L to R): ' + (Images.length-1));
-                Images[index - Images.length].className         = "slideImg slideOut-LtoR";
+
+                Images[index - Images.length].className = "slideImg slideOut-LtoR";
+                Caption[index - Images.length].className = "imgCaption hidden";
                 console.log('Out (L to R): ' + (index));
             } else {
-                Images[index - 1].className         = "slideImg slideIN-LtoR";
+                Images[index - 1].className             = "slideImg slideIN-LtoR";
+                Caption[index - 1].className            = "imgCaption caption-In";
                 console.log('In (L to R): ' + (index-1));
-                Images[index].className            = "slideImg slideOut-LtoR";
+
+                Images[index].className                 = "slideImg slideOut-LtoR";
+                Caption[index].className                = "imgCaption hidden";
                 console.log('Out (L to R): ' + (index));
             }
         }
